62ea77c8a5a3392db19808754c24c3cf5e33b6d3
Dongwon Paek <ikarus125@gmail.com>
2020-01-21 19:03:21 +0900

    Update tfCNNex.py

--- Python/CNNexample/tfCNNex.py
+++ Python/CNNexample/tfCNNex.py
@@ -76,7 +76,7 @@ def time_tensorflow_run(session, target, info_string):
     total_duration = 0.0
     total_duration_squared = 0.0
 
-    for i in range(num_batches + num_steps_burn_in):
+    for i in range(num_batch + num_steps_burn_in):
         start_time = time.time()
         _ = session.run(target)
         duration = time.time() - start_time
@@ -86,15 +86,15 @@ def time_tensorflow_run(session, target, info_string):
             total_duration += duration
             total_duration_squared += duration ** 2
 
-    mn = total_duration / num_batches
-    vr = total_duration_squared / num_batches - mn ** 2
+    mn = total_duration / num_batch
+    vr = total_duration_squared / num_batch - mn ** 2
     sd = math.sqrt(vr)
     print('%s: %s across %d steps, %.3f +/- %.3f sec / batch' % (datetime.now(), info_string, num_batches, mn, sd))
 
 def run_benchmark():
     with tf.Graph().as_default():
         image_size = 224
-        images = tf.Variable(tf.random_normal([batch_size, image_size, image_size, 3], dtype = tf.float32, stddev = 1e-1))
+        images = tf.Variable(tf.random.normal([batch_size, image_size, image_size, 3], dtype = tf.float32, stddev = 1e-1))
         pool5, parameters = inference(images)
 
         init = tf.global_variables_initializer()
